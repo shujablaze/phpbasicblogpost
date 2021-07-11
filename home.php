38b599532d66fa6ceb9a1bbc75108b0c3cd9e41a
Shuja Hamid <85234536+shujablaze@users.noreply.github.com>
2021-07-11 15:07:28 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Chumt Blog</title>
+</head>
+<body>
+    <div class="blog-container">
+        <?php
+            $conn=mysqli_connect('localhost','root','password786','chumt');
+            if(!mysqli_connect_errno()){
+                $sql = "SELECT * FROM blogs";
+                $result = mysqli_query($conn,$sql);
+                $blogs = mysqli_fetch_all($result,MYSQLI_ASSOC);
+            }
+            foreach($blogs as $blog):
+        ?>
+        <div class="blog">
+            <div class="blog__title"><?php echo $blog["title"]; ?></div>
+            <div class="inner-container">
+                <img src="<?php echo $blog["img"]; ?>" alt="chumt kumar" class="blog__img">
+                <div class="blog__text"><?php echo $blog["content"]; ?></div>
+            </div>
+        </div>
+            <?php endforeach; ?>
+    </div>
+</body>
+</html>
+
+
